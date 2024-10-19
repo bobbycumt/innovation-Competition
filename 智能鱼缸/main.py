fedf5b64059c7aca63feb3535fddcf83ad634fb2
bobbycumt <bobbycumt@163.com>
2024-10-19 13:42:49 +0800

    new

---
+++ 智能鱼缸/main.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from machine import Pin,I2C,ADC
+import utime
+# from lib import urequests
+import ujson
+# import ssd1306
+from umqtt.simple import MQTTClient
+
+hj=ADC(34)
+level=ADC(35)
+pmp1 = Pin(12, Pin.OUT)
+pmp2 = Pin(13, Pin.OUT)
+motor = Pin(14, Pin.OUT)
+
+# ssid='CU_future'
+# psw='13582579999'
+ssid = 'bbhh'
+psw = 'lb19850922'
+
+SERVER = "bj-2-mqtt.iot-api.com"
+PORT = 1883
+CLIENT_ID = 'sr6avzfo'
+USERNAME = '2dkx0lasne3bi7ak'
+PASSWORD = '85mf2Hkk0p'
+TOPIC = "attributes"
+TOPIC1 = "attributes/push"
+
+f=0
+state=0
+utime.sleep(2)
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, psw)
+        while not wlan.isconnected():
+             pass
+    print('network config:', wlan.ifconfig())
+
+def mqtt_connect():
+    client = MQTTClient(CLIENT_ID, SERVER, PORT, USERNAME, PASSWORD)
+    client.connect()
+    print('Connected to MQTT Broker "{server}"'.format(server = SERVER))
+    return client
+
+def on_message(topic, msg):
+    global f
+    global state
+    print("Received '{payload}' from topic '{topic}'\n".format(
+        payload = msg.decode(), topic = topic.decode()))
+    d=ujson.loads(msg.decode())
+    k=list(d.keys())
+    if k[0]=="pump1":
+        pmp1.value(d["pump1"])
+    elif k[0]=='pump2':   
+        pmp2.value(d["pump2"])
+    elif k[0]=='motor':   
+        motor.value(d["motor"])
+        
+def subscribe(client):
+    client.set_callback(on_message)
+    client.subscribe(TOPIC1)
+
+def main():
+    global f
+    cnt=0
+    do_connect()
+    client= mqtt_connect()
+    subscribe(client)
+    while 1:
+        client.check_msg()
+        utime.sleep_ms(200)
+        cnt+=1
+        if cnt==50:
+            msg_dict = {
+                'turbidity': hj.read(),'level': level.read()
+            }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            cnt=0
+#         if f==0:
+#             client.check_msg()
+#             client.ping()
+#             utime.sleep_ms(200)
+
+if __name__=='__main__':
+    main()
---
+++ 智能鱼缸/oled_show.py
@@ -0,0 +1,36 @@
+'''
+实验名称：【我的学习系统】之OLED中文显示（I2C总线）
+版本：v1.0
+日期：2021.6.5
+作者：kaixindelele
+'''
+
+from machine import I2C,Pin         #从machine模块导入I2C、Pin子模块
+from ssd1306 import SSD1306_I2C     #从ssd1306模块中导入SSD1306_I2C子模块
+import utime
+
+
+i2c = I2C(sda=Pin(21), scl=Pin(22))   #pyBoard I2C初始化：sda--> Y8, scl --> Y6
+oled = SSD1306_I2C(128, 64, i2c, addr=0x3c) #OLED显示屏初始化：128*64分辨率,OLED的I2C地址是0x3c
+print(12)
+oled.poweroff()
+utime.sleep_ms(50)
+oled.poweron()
+utime.sleep_ms(50)
+oled.font_load("GB2312-12.fon")
+oled.fill(0)
+# help(oled.font_set)
+
+
+oled.text("唐山天气", 0,  0)      #写入第1行内容
+oled.line(0, 14, 128, 14, 1)
+# oled.text("手机已使用时间:3H25M",  0, 16)      #写入第2行内容
+# 
+# oled.text("本次剩余时间:0H21M",  0, 30)      #写入第3行内容
+# 
+# oled.text("一定要好好学习啊!",  0, 44)
+
+
+oled.show()   #OLED执行显示
+
+
